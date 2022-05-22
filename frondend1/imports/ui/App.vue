2b1833e95f8a35f1311e3e17912734746b3da669
Karo0510 <karma0510@gmail.com>
2022-05-22 15:30:33 +0200

    pierwsza wersja frondend

---
+++ frondend1/imports/ui/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+   <h1>adam</h1>
+      <header>
+        <router-link to="/login">
+          Login
+        </router-link>
+        <router-link to="/register">
+          Register
+        </router-link>
+        <router-view></router-view>
+      </header>
+
+  </v-app>
+</template>
+
+
+
+<script>
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+
+export default {
+};
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ frondend1/imports/ui/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <center>
+      <h1 class="h3 mb-3 fw-normal">Sign in</h1>
+
+      <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <div>
+
+    <p class="mt-5 mb-3 text-muted">@Create for Student Project</p>
+    <p class="authors"><strong>Authors Project:</strong><br>Karolina Maciejewska <a href="https://github.com/Karo0510">Karolina Github</a> <br>Maciej Morawski <a href="https://github.com/MacMarMor">Maciej Github</a><br>Adam Grzesiak <a href="https://github.com/adamg177">Adam Github</a></p>
+    </div>
+      
+    </center>
+
+
+</template>
+
+<script>
+    export default{
+    }
+ </script>
+   
+ <style>
+
+</style>
